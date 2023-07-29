cc99fe1487154b827fe874e778cc336d75a123a0
Mochizuki Haruka <mh@owari.org>
2023-07-29 14:10:24 +0800

    ALL

--- src/main/kotlin/org/owari/akari/tech/datagen/Model.kt
+++ src/main/kotlin/org/owari/akari/tech/datagen/Model.kt
@@ -12,11 +12,36 @@ import java.util.*
 class ModelProvider(gen: FabricDataGenerator) : FabricModelProvider(gen) {
     override fun generateBlockStateModels(gen: BlockStateModelGenerator) {
         gen.registerSimpleCubeAll(Blocks.ORE_TIN)
+        gen.registerSimpleCubeAll(Blocks.ORE_ZINC)
+        gen.registerSimpleCubeAll(Blocks.ORE_LEAD)
+        gen.registerSimpleCubeAll(Blocks.ORE_SILVER)
+        gen.registerSimpleCubeAll(Blocks.ORE_MANGANESE)
+        gen.registerSimpleCubeAll(Blocks.ORE_NICKEL)
+        gen.registerSimpleCubeAll(Blocks.ORE_MAGNESIUM)
+        gen.registerSimpleCubeAll(Blocks.ORE_CHROMIUM)
+        gen.registerSimpleCubeAll(Blocks.ORE_RUTILE)
+        gen.registerSimpleCubeAll(Blocks.ORE_TITANIUM)
+        gen.registerSimpleCubeAll(Blocks.ORE_TUNGSTEN)
+        gen.registerSimpleCubeAll(Blocks.ORE_PLATINUM_GROUP)
+        // 黄铁矿 (愚人金) 注册为金矿的材质
+        gen.registerSingleton(Blocks.ORE_PYRITE, TextureMap.all(Identifier("minecraft", "block/gold_ore")), Models.CUBE_ALL)
+        gen.registerSimpleCubeAll(Blocks.ORE_SULFUR)
+        gen.registerSimpleCubeAll(Blocks.ORE_APATITE)
+        gen.registerSimpleCubeAll(Blocks.ORE_RUBY)
+        gen.registerSimpleCubeAll(Blocks.ORE_ANTIMONY)
+        gen.registerSimpleCubeAll(Blocks.ORE_LITHIUM)
+        gen.registerSimpleCubeAll(Blocks.ORE_GRAPHITE)
+        gen.registerSimpleCubeAll(Blocks.ORE_NITERITE)
+        gen.registerSimpleCubeAll(Blocks.ORE_URANIUM)
+
+        gen.registerSimpleCubeAll(Blocks.ORE_ALUMINIUM)
+        gen.registerSimpleCubeAll(Blocks.ORE_BLACK_SAND)
+        gen.registerSimpleCubeAll(Blocks.ORE_ANTHRACITE)
+        gen.registerSimpleCubeAll(Blocks.ORE_SALT)
+        gen.registerSimpleCubeAll(Blocks.ORE_ROCK_SALT)
     }
 
     override fun generateItemModels(gen: ItemModelGenerator) {
         gen.register(Items.INGOT_TIN, Models.GENERATED)
-
-        gen.register(Blocks.ORE_TIN_ITEM, Model(Optional.of(Identifier(modId, "block/ore_tin")), Optional.empty()))
     }
 }
--- src/main/kotlin/org/owari/akari/tech/worldgen/OreGenerators.kt
+++ src/main/kotlin/org/owari/akari/tech/worldgen/OreGenerators.kt
@@ -3,6 +3,8 @@ package org.owari.akari.tech.worldgen
 import net.fabricmc.fabric.api.biome.v1.BiomeModification
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications
 import net.fabricmc.fabric.api.biome.v1.BiomeSelectors
+import net.minecraft.block.Block
+import net.minecraft.structure.rule.RuleTest
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.*
 import net.minecraft.world.gen.GenerationStep
@@ -16,7 +18,7 @@ import org.owari.akari.tech.modId
 
 object OreGenerators {
     fun registerOreGenerators() {
-        register("ore_tin", CONFIG_FEATURE_ORE_TIN, PLACE_FEATURE_ORE_TIN)
+        register("ore_tin", CF_ORE_TIN, PF_ORE_TIN)
     }
 
     private inline fun register(name: String, cf: ConfiguredFeature<*, *>, pf: PlacedFeature) {
@@ -25,24 +27,12 @@ object OreGenerators {
         Registry.register(BuiltinRegistries.PLACED_FEATURE, id, pf)
         BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, RegistryKey.of(Registry.PLACED_FEATURE_KEY, id))
     }
+
+    private inline fun oreConfigFeature(block: Block, size: Int, test: RuleTest = OreConfiguredFeatures.STONE_ORE_REPLACEABLES) =
+        ConfiguredFeature(Feature.ORE, OreFeatureConfig(test, block.defaultState, size))
+    private inline fun orePlacedFeature(cf: ConfiguredFeature<*, *>, count: Int, top: YOffset = YOffset.TOP, bottom: YOffset = YOffset.BOTTOM) =
+        PlacedFeature(RegistryEntry.of(cf), listOf(CountPlacementModifier.of(count), SquarePlacementModifier.of(), HeightRangePlacementModifier.uniform(top, bottom)))
+
+    @JvmStatic val CF_ORE_TIN = oreConfigFeature(Blocks.ORE_TIN, 6)
+    @JvmStatic val PF_ORE_TIN = orePlacedFeature(CF_ORE_TIN, 20, YOffset.fixed(64))
 }
-
-val CONFIG_FEATURE_ORE_TIN = ConfiguredFeature(
-    Feature.ORE, OreFeatureConfig(
-        OreConfiguredFeatures.STONE_ORE_REPLACEABLES,
-        Blocks.ORE_TIN.defaultState,
-        6
-    )
-)
-
-val PLACE_FEATURE_ORE_TIN = PlacedFeature(
-    RegistryEntry.of(CONFIG_FEATURE_ORE_TIN),
-    listOf(
-        CountPlacementModifier.of(20),
-        SquarePlacementModifier.of(),
-        HeightRangePlacementModifier.uniform(
-            YOffset.BOTTOM,
-            YOffset.fixed(64)
-        )
-    )
-)
